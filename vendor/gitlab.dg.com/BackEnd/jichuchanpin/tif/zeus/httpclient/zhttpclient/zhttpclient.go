8c845373a2d0fccc805f566fbf1a401cb5960f82
markmai <markmai@digitalgd.com.cn>
2020-12-30 13:26:02 +0800

    Add # 增加vendor依赖目录

---
+++ vendor/gitlab.dg.com/BackEnd/jichuchanpin/tif/zeus/httpclient/zhttpclient/zhttpclient.go
@@ -0,0 +1,19 @@
+package zhttpclient
+
+import (
+	"context"
+	"io"
+)
+
+type HttpClient interface {
+	GetHttpClient(instance string) (Client, error)
+}
+
+type Client interface {
+	Get(ctx context.Context, url string, headers map[string]string) ([]byte, error)
+	Post(ctx context.Context, url string, body io.Reader, headers map[string]string) ([]byte, error)
+    PostWithStatusCode(ctx context.Context, url string, body io.Reader, headers map[string]string) (rsp []byte, s int, err error)
+	Put(ctx context.Context, url string, body io.Reader, headers map[string]string) ([]byte, error)
+	Patch(ctx context.Context, url string, body io.Reader, headers map[string]string) ([]byte, error)
+	Delete(ctx context.Context, url string, headers map[string]string) ([]byte, error)
+}
